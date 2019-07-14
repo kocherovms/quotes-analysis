87c809074076c9e87628160cb749bf258ea7a641
Michael Kocherov <m@develorium.com>
2019-07-14 23:53:22 +0300

    stock_list

---
+++ main.py
@@ -0,0 +1,32 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+from lib.stock_list import stock_list
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Seasonality analysis of stock quotes'),
+    dcc.Dropdown(
+        id='stock-code',
+        options=[{'label': '{} / {} ({})'.format(stock_list[e]['name'], stock_list[e]['type'], e), 'value': e} for e in stock_list]
+    ),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
